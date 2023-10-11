b83e294f244e3d4d0994cb101734e79ec8f651be
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-10-11 20:29:21 +0300

    feat: add description page

--- open-table-nextjs/src/app/page.tsx
+++ open-table-nextjs/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           </a>
           <div>
             <div className='flex gap-3'>
-              <button className='bg-blue-900 border p-1 px-4 rounded'>
+              <button className='bg-blue-900 border p-1 px-4 rounded text-white'>
                 Sign In
               </button>
               <button className='text-black border p-1 px-4 rounded'>
@@ -24,17 +24,59 @@ export default function Home() {
           </div>
         </nav>
         {/* navbar */}
-        <main>
-          {/* header */}
-          <div className='h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2'>
-            <div className='text-center mt-10'>
-              <h1 className='text-5xl font-bold mb-2'>
-                Find your table for any ocassions
-              </h1>
-            </div>
+        {/* header */}
+        <div className='h-96 overflow-hidden'>
+          <div className='bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center'>
+            <h1 className='text-7xl text-white capitalized text-shadow text-center'>
+              Milestones Grill (Toronto)
+            </h1>
           </div>
-          {/* header */}
-        </main>
+        </div>
+        {/* header */}
+        {/* description portion */}
+        <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
+          <div className='bg-white w-[70%] rounded p-3 shadow'>
+            {/* restaurants navbar */}
+            <nav className='flex text-reg border-b pb-2'>
+              <a href='' className='mr-7'>
+                Overview
+              </a>
+              <a href='' className='mr-7'>
+                Menu
+              </a>
+            </nav>
+            {/* restaurants navbar */}
+            {/* title */}
+            <div className='mt-4 border-b pb-6'>
+              <h1 className='font-bold text-6xl'>Milestones Grill</h1>
+            </div>
+            {/* title */}
+            {/* ratings */}
+            <div className='flex items-end'>
+              <div className='ratings mt-2 flex items-center'>
+                <p>*****</p>
+                <p className='text-reg ml-3'>4.9</p>
+              </div>
+              <div>
+                <p className='text-reg ml-4'>600 Reviews</p>
+              </div>
+            </div>
+            {/* ratings */}
+            {/* description */}
+            <div className='mt-4'>
+              <p className='text-lg font-light'>
+                The classics you love prepared with a perfect twist, all served
+                up in an atmosphere that feels just right. That’s the Milestones
+                promise. So, whether you’re celebrating a milestone, making the
+                most of Happy Hour or enjoying brunch with friends, you can be
+                sure that every Milestones experience is a simple and perfectly
+                memorable one.
+              </p>
+            </div>
+            {/* description */}
+          </div>
+        </div>
+        {/* description portion */}
       </main>
     </main>
   );
