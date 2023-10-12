cafe9426fd53a14e68edaea346a999845890f7ed
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-10-12 12:58:12 +0300

    feat: add description left side

--- open-table-nextjs/src/app/page.tsx
+++ open-table-nextjs/src/app/page.tsx
@@ -5,19 +5,19 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className='bg-gray-100 min-h-screen w-screen'>
-      <main className='max-w-screen-2xl m-auto bg-white'>
+    <main className="bg-gray-100 min-h-screen w-screen">
+      <main className="max-w-screen-2xl m-auto bg-white">
         {/* navbar */}
-        <nav className='bg-white p-2 flex justify-between'>
-          <a href='' className='font-bold text-gray-700 text-2xl'>
+        <nav className="bg-white p-2 flex justify-between">
+          <a href="" className="font-bold text-gray-700 text-2xl">
             OpenTable
           </a>
           <div>
-            <div className='flex gap-3'>
-              <button className='bg-blue-900 border p-1 px-4 rounded text-white'>
+            <div className="flex gap-3">
+              <button className="bg-blue-900 border p-1 px-4 rounded text-white">
                 Sign In
               </button>
-              <button className='text-black border p-1 px-4 rounded'>
+              <button className="text-black border p-1 px-4 rounded">
                 Sign Up
               </button>
             </div>
@@ -25,46 +25,46 @@ export default function Home() {
         </nav>
         {/* navbar */}
         {/* header */}
-        <div className='h-96 overflow-hidden'>
-          <div className='bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center'>
-            <h1 className='text-7xl text-white capitalized text-shadow text-center'>
+        <div className="h-96 overflow-hidden">
+          <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
+            <h1 className="text-7xl text-white capitalized text-shadow text-center">
               Milestones Grill (Toronto)
             </h1>
           </div>
         </div>
         {/* header */}
         {/* description portion */}
-        <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-          <div className='bg-white w-[70%] rounded p-3 shadow'>
+        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+          <div className="bg-white w-[70%] rounded p-3 shadow">
             {/* restaurants navbar */}
-            <nav className='flex text-reg border-b pb-2'>
-              <a href='' className='mr-7'>
+            <nav className="flex text-reg border-b pb-2">
+              <a href="" className="mr-7">
                 Overview
               </a>
-              <a href='' className='mr-7'>
+              <a href="" className="mr-7">
                 Menu
               </a>
             </nav>
             {/* restaurants navbar */}
             {/* title */}
-            <div className='mt-4 border-b pb-6'>
-              <h1 className='font-bold text-6xl'>Milestones Grill</h1>
+            <div className="mt-4 border-b pb-6">
+              <h1 className="font-bold text-6xl">Milestones Grill</h1>
             </div>
             {/* title */}
             {/* ratings */}
-            <div className='flex items-end'>
-              <div className='ratings mt-2 flex items-center'>
+            <div className="flex items-end">
+              <div className="ratings mt-2 flex items-center">
                 <p>*****</p>
-                <p className='text-reg ml-3'>4.9</p>
+                <p className="text-reg ml-3">4.9</p>
               </div>
               <div>
-                <p className='text-reg ml-4'>600 Reviews</p>
+                <p className="text-reg ml-4">600 Reviews</p>
               </div>
             </div>
             {/* ratings */}
             {/* description */}
-            <div className='mt-4'>
-              <p className='text-lg font-light'>
+            <div className="mt-4">
+              <p className="text-lg font-light">
                 The classics you love prepared with a perfect twist, all served
                 up in an atmosphere that feels just right. That’s the Milestones
                 promise. So, whether you’re celebrating a milestone, making the
@@ -74,6 +74,74 @@ export default function Home() {
               </p>
             </div>
             {/* description */}
+            {/* images */}
+            <div>
+              <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">
+                5 photos
+              </h1>
+              <div className="flex flex-wrap">
+                <img
+                  className="w-56 h-44 mr-1 mb-1"
+                  src="https://resizer.otstatic.com/v2/photos/xlarge/3/41701449.jpg"
+                  alt=""
+                />
+                <img
+                  className="w-56 h-44 mr-1 mb-1"
+                  src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701450.jpg"
+                  alt=""
+                />
+                <img
+                  className="w-56 h-44 mr-1 mb-1"
+                  src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701452.jpg"
+                  alt=""
+                />
+                <img
+                  className="w-56 h-44 mr-1 mb-1"
+                  src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701453.jpg"
+                  alt=""
+                />
+                <img
+                  className="w-56 h-44 mr-1 mb-1"
+                  src="https://resizer.otstatic.com/v2/photos/xlarge/2/41701454.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+            {/* images */}
+            {/* reviews */}
+            <div>
+              <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">
+                What 100 people are saying
+              </h1>
+              <div>
+                {/* reviewcard */}
+                <div className="border-b pb-7 mb-7">
+                  <div className="flex">
+                    <div className="w-1/6 flex flex-col items-center">
+                      <div className="rounded-full bg-blue-400 w-16 h-16 flex items-center justify-center">
+                        <h2 className="text-white etxt-2xl">MJ</h2>
+                      </div>
+                      <p className="text-center">Michal Jordan</p>
+                    </div>
+                    <div className="ml-10 w-5/6">
+                      <div className="flex items-center">
+                        <div className="flex mr-5">*****</div>
+                      </div>
+                      <div className="mt-5">
+                        <p className="text-lg font-light">
+                          Laurie was on top of everything! Slow night due to the
+                          snow storm so it worked in our favor to have more one
+                          on one with the staff. Delicious and well worth the
+                          money.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* reviewcard */}
+              </div>
+            </div>
+            {/* reviews */}
           </div>
         </div>
         {/* description portion */}
