6ecf01d0e3d03e14634fde0d4fae3c7d6af2c169
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-10-16 11:30:31 +0300

    feat: add layout

--- open-table-nextjs/src/app/restaurant/[slug]/menu/page.tsx
+++ open-table-nextjs/src/app/restaurant/[slug]/menu/page.tsx
@@ -1,23 +1,19 @@
 import React from "react";
-import NavBar from "@/app/components/NavBar";
 import NavBarMenu from "../../components/NavBarMenu";
 import Header from "../../components/Header";
 import RestaurantNav from "../../components/RestaurantNav";
 
 const RestaurantDetailsPageMenu = () => {
   return (
-    <main className='bg-gray-100 min-h-screen w-screen'>
-      <main className='max-w-screen-2xl m-auto bg-white'>
-        <NavBar />
-        <Header />
-        <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-          <div className='bg-white w-[100%] rounded p-3 shadow'>
-            <RestaurantNav />
-            <NavBarMenu />
-          </div>
+    <>
+      <Header />
+      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+        <div className="bg-white w-[100%] rounded p-3 shadow">
+          <RestaurantNav />
+          <NavBarMenu />
         </div>
-      </main>
-    </main>
+      </div>
+    </>
   );
 };
 
--- open-table-nextjs/src/app/restaurant/[slug]/page.tsx
+++ open-table-nextjs/src/app/restaurant/[slug]/page.tsx
@@ -1,17 +1,13 @@
 import React from "react";
-import NavBar from "@/app/components/NavBar";
 import Header from "../components/Header";
 import Description from "../components/Description";
 
 const RestaurantDetailsPage = () => {
   return (
-    <main className='bg-gray-100 min-h-screen w-screen'>
-      <main className='max-w-screen-2xl m-auto bg-white'>
-        <NavBar />
-        <Header />
-        <Description />
-      </main>
-    </main>
+    <div>
+      <Header />
+      <Description />
+    </div>
   );
 };
 
