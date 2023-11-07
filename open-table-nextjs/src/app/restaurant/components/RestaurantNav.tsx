1811e538d8770cb196ea79dd73df03f88df610f2
Mikhail Matveichuk <mihkailmatveichuk@gmail.com>
2023-11-07 19:19:13 +0300

    feat: add api env

--- open-table-nextjs/src/app/restaurant/components/RestaurantNav.tsx
+++ open-table-nextjs/src/app/restaurant/components/RestaurantNav.tsx
@@ -1,13 +1,34 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const RestaurantNav = ({ slug }: { slug: string }) => {
+  const pathname = usePathname();
+
   return (
     <nav className='flex text-reg border-b pb-2'>
-      <Link href={`/restaurant/${slug}`} className='mr-7'>
+      <Link
+        href={`/restaurant/${slug}`}
+        className='mr-7'
+        style={
+          pathname === `/restaurant/${slug}`
+            ? { fontWeight: 500 }
+            : { fontWeight: 300 }
+        }
+      >
         Overview
       </Link>
-      <Link href={`/restaurant/${slug}/menu`} className='mr-7'>
+      <Link
+        href={`/restaurant/${slug}/menu`}
+        className='mr-7'
+        style={
+          pathname === `/restaurant/${slug}/menu`
+            ? { fontWeight: 500 }
+            : { fontWeight: 300 }
+        }
+      >
         Menu
       </Link>
     </nav>
